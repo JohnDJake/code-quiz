7db032d141fcf0a6d5955874b5af7a763cc3af31
JohnDJake <johndjake@gmail.com>
2020-12-10 23:40:22 -0800

    added basic timer functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,19 @@
+var timer = 60;
+var timerSpan = document.querySelector("#timer");
+
 // Hide the start page and show the quiz page when the start button is clicked, then start the quiz
-document.querySelector("#start").addEventListener("click", function() {
+document.querySelector("#start").addEventListener("click", function () {
     document.querySelector("#start-page").style.display = "none";
     document.querySelector("#quiz-page").style.display = "block";
-    startQuiz();
+
+    // start the timer
+    var timerInterval = setInterval(function () {
+        // decrement the timer and update the html timer
+        timerSpan.textContent = --timer;
+        if (timer <= 0) {
+            clearInterval(timerInterval);
+        }
+    }, 1000);
+
+    // TODO start the quiz
 })
-
-// TODO write startQuiz() function
-function startQuiz() {
-
-}
