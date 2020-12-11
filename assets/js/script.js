c1d716836b70c1f5d848579b58c531d0ab8d9c3d
JohnDJake <johndjake@gmail.com>
2020-12-10 22:45:42 -0800

    the start button now hides the start page and shows the quiz page

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Hide the start page and show the quiz page when the start button is clicked, then start the quiz
+document.querySelector("#start").addEventListener("click", function() {
+    document.querySelector("#start-page").style.display = "none";
+    document.querySelector("#quiz-page").style.display = "block";
+    startQuiz();
+})
+
+// TODO write startQuiz() function
+function startQuiz() {
+
+}
