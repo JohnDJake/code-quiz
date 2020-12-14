ca47a166f2fcaa62dbbb6bc06f96889c85bb488b
JohnDJake <johndjake@gmail.com>
2020-12-13 22:39:28 -0800

    added code to update page with questions and handle answers, with placeholder questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,36 @@
+const timerSpan = document.querySelector("#timer");
+const startPageEl = document.querySelector("#start-page");
+const quizPageEl = document.querySelector("#quiz-page");
+const questionEl = document.querySelector("#question");
+const answer0El = document.querySelector("#answer0");
+const answer1El = document.querySelector("#answer1");
+const answer2El = document.querySelector("#answer2");
+const answer3El = document.querySelector("#answer3");
+
+
+var questions = [{
+    question: "lajerfajejsdf",
+    answers: ["aerigj", "alkjef", "akjsrf", "aosejf"],
+    correctAnswer: "akjsrf"
+}, {
+    question: "jaergljhjk",
+    answers: ["ntrewglkj", "kajergnkj", "relkrtjhb", "kaejrng"],
+    correctAnswer: "ntrewglkj"
+}]
+
 var timer = 60;
-var timerSpan = document.querySelector("#timer");
+var questionIndex = -1;
+
+// Shuffle the questions and the answers
+shuffleArray(questions);
+for (var i = 0; i < questions.length; i++) {
+    shuffleArray(questions[i].answers);
+}
 
 // Hide the start page and show the quiz page when the start button is clicked, then start the quiz
 document.querySelector("#start").addEventListener("click", function () {
-    document.querySelector("#start-page").style.display = "none";
-    document.querySelector("#quiz-page").style.display = "block";
+    startPageEl.style.display = "none";
+    quizPageEl.style.display = "block";
 
     // start the timer
     var timerInterval = setInterval(function () {
@@ -12,8 +38,47 @@ document.querySelector("#start").addEventListener("click", function () {
         timerSpan.textContent = --timer;
         if (timer <= 0) {
             clearInterval(timerInterval);
+            endGame();
         }
     }, 1000);
 
-    // TODO start the quiz
+    // start the quiz
+    nextQuestion();
+    document.querySelectorAll(".answer").forEach(function(el) {
+        el.addEventListener("click", function(event) {
+        if (event.target.textContent != questions[questionIndex].correctAnswer) {
+            timer -= 10;
+        }
+        nextQuestion();
+    })
+    })
 })
+
+// Switch to the next question
+function nextQuestion() {
+    if (++questionIndex == questions.length) {
+        endGame();
+        return;
+    }
+    questionEl.textContent = questions[questionIndex].question;
+    answer1El.textContent = questions[questionIndex].answers[1];
+    answer2El.textContent = questions[questionIndex].answers[2];
+    answer0El.textContent = questions[questionIndex].answers[0];
+    answer3El.textContent = questions[questionIndex].answers[3];
+}
+
+// Randomly shuffle an array
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * i);
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+// End the quiz
+// TODO write some code
+function endGame() {
+    quizPageEl.style.display = "none";
+}
